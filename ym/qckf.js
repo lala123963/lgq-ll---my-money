09b83c1e956b3592be1c40174eaa227d6678fb7c
13735279771 <57555508+13735279771@users.noreply.github.com>
2022-06-17 11:17:48 +0800

    Update qckf.js

--- ym/qckf.js
+++ ym/qckf.js
@@ -24,7 +24,7 @@
  * 
  */
 
-const $ = new Env("雀巢咖啡");
+const $ = new Env("微信小程序-雀巢咖啡");
 const notify = $.isNode() ? require("./sendNotify") : "";
 const Notify = 1 		//0为关闭通知,1为打开通知,默认为1
 const debug = 0 		//0为关闭调试,1为打开调试,默认为0
