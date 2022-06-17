02a0e58d9bb2690bebb7fd462ad8a02ad385250f
13735279771 <57555508+13735279771@users.noreply.github.com>
2022-06-17 11:18:03 +0800

    Update xlh.js

--- ym/xlh.js
+++ ym/xlh.js
@@ -21,7 +21,7 @@
  * 还是不会的请百度或者群里求助：QQ群：884234287  tg：https://t.me/yml_tg
  */
 
-const $ = new Env("骁龙会");
+const $ = new Env("微信小程序-骁龙会");
 const notify = $.isNode() ? require('./sendNotify') : '';
 const Notify = 1; //0为关闭通知，1为打开通知,默认为1
 const debug = 0; //0为关闭调试，1为打开调试,默认为0
